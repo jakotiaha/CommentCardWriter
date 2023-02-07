35d07ef39304add2558478ad164a5734983a5ac1
Akshat Jakotiah <jakotiah.a@etoncollege.org.uk>
2023-02-07 10:09:23 +0000

    Initial Commit

---
+++ Comment Card Writer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Comment Card Writer
+//
+//  Created by Jakotiah, Akshat (PGW) on 07/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
